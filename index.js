5998514eea08cc1fdb8eeef226e019774ee14f96
Siddharth Goutham <siddhartha.nalluraya@gmail.com>
2026-02-26 16:29:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const phoneBtn = document.getElementById("phone-no-button");
+const phoneBox = document.getElementById("phoneBox");
+
+phoneBtn.addEventListener("click", () => {
+    phoneBox.classList.toggle("show");
+});
+
+const emailBtn = document.getElementById("email-button");
+const emailBox = document.getElementById("emailBox");
+
+emailBtn.addEventListener("click", () => {
+    emailBox.classList.toggle("show");
+});
+
+const viewBtn = document.getElementById("view-projects");
+
+viewBtn.addEventListener("click", () => {
+    document.getElementById("featured-projects").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+const contactBtn=document.getElementById("contact");
+contactBtn.addEventListener("click", () => {
+    document.getElementById("contact-heading").scrollIntoView({
+        behavior: "smooth"
+    });
+});
