d8eb0db20590b516d88b687a6236e49da254471a
Siddharth Goutham <siddhartha.nalluraya@gmail.com>
2026-02-27 16:29:44 +0530

    Refactor event listeners and add custom cursor

--- index.js
+++ index.js
@@ -26,3 +26,10 @@ contactBtn.addEventListener("click", () => {
         behavior: "smooth"
     });
 });
+
+const cursor = document.querySelector(".custom-cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
